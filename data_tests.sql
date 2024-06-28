a7c7ab3c421086f1c816825f2c5f8e522c485a75
swateetjx <samairasv1920@gmail.com>
2024-06-28 20:13:52 +0530

    Update data_tests.sql

--- data_tests.sql
+++ data_tests.sql
@@ -1,2 +1,5 @@
 --Test for Null Values
 select COUNT(*) AS column_null_count from DEV_EIM_DB.PID.FIRST_TABLE_CICD where LNAME IS NULL;
+
+--Test for invalid data
+select COUNT(*) AS negative_metric_count from DEV_EIM_DB.PID.SECOND_TABLE_CICD where AGE < 0;
