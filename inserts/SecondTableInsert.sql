ac3971efe5cbf598798b46ff8fab6e72bb26acbb
swateetjx <samairasv1920@gmail.com>
2024-06-28 20:02:11 +0530

    Update SecondTableInsert.sql

--- inserts/SecondTableInsert.sql
+++ inserts/SecondTableInsert.sql
@@ -1,6 +1,4 @@
 INSERT INTO 
 %DBNAME%.PID.SECOND_TABLE_CICD (FIRSTNAME,LASTNAME,GENDER,AGE)
 VALUES 
-('Will','Smith','M',NULL),
-('Robin','Sharma','M',-1),
-('John','K','M',28);
+('Alia',,'F',25);
