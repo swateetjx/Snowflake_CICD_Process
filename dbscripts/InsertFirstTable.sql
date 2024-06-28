ce6cbb207215944c367b6f54535b1fb7433890d0
swateetjx <samairasv1920@gmail.com>
2024-06-28 12:27:35 +0530

    Update InsertFirstTable.sql

--- dbscripts/InsertFirstTable.sql
+++ dbscripts/InsertFirstTable.sql
@@ -1,6 +1,6 @@
 INSERT INTO TABLE
-DEV_EIM_DB.PID.FIRST_TABLE_CICD
+DEV_EIM_DB.PID.FIRST_TABLE_CICD (FNAME,LNAME)
 VALUES 
-("Swatee","Singh")
-("Revathi","Velu")
-("Raksha","Shenoy")
+("Swatee","Singh"),
+("Revathi","Velu"),
+("Raksha","Shenoy");
